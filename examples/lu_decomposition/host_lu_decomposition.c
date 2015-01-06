20592d3bb34b8022b203835f74d66dfebcded048
Jan-Willem Buurlage <jwbuurlage@me.com>
2015-01-06 08:31:06 +0100

    Progress on LU decomposition

--- examples/lu_decomposition/host_lu_decomposition.c
+++ examples/lu_decomposition/host_lu_decomposition.c
@@ -3,31 +3,37 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-// distribution function, returns proc id
-int phi(i, j, N, M)
+#include "common.h"
+
+// information on matrix and procs
+char N = -1;
+char M = -1;
+int k = -1;
+
+// "local to global" index
+int ltg(int& i, int& j, int l, int s, int t)
 {
-    
+    rows_per_proc = k / N;
+    cols_per_proc = k / M;
+    i = (l / cols_per_proc) * rows_per_proc + rows_per_proc % pid + s; // FIXME
+    j = (l / cols_per_proc) * rows_per_proc + rows_per_proc % pid + t; // FIXME
 }
 
-// Functions to convert between global and local coordinates
-// Local to global
-int ltg(int i, int j, int& l)
+// "global to local" index
+int gtl(int i, int j, int& l)
 {
-
-}
-
-// Global to local
-int ltg(int& i, int& j, int l)
-{
-
+    l = i % M + 3; // FIXME
 }
 
 int main(int argc, char **argv)
 {
     // for our example we decompose the (k x k) Hilbert matrix
-    int k = 10;
+    k = 10;
+
+    // allocate and zero-initialize matrix
     float* hilb = calloc(sizeof(float) * k * k);
 
+    // construct the matrix H, which is defined as H_ij = 1/(i+j-1)
     int i = 0; 
     int j = 0;
     for(i = 1; i <= k; ++i) {
@@ -36,12 +42,11 @@ int main(int argc, char **argv)
         }
     }
 
+    // initialize the BSP system
     bsp_init("bin/e_lu_decomposition.srec", argc, argv);
     bsp_begin(bsp_nprocs());
 
     // distribute the matrix
-    int N = 0;
-    int M = 0;
     switch(bsp_nprocs()) {
         case 16:
             N = 4;
@@ -59,13 +64,20 @@ int main(int argc, char **argv)
             break;
     }
 
+    // Write M to every processor such that they can figure out the (s,t) pair
+    for(i = 0; i < bsp_nprocs(); ++i) {
+        co_write(i, M, LOC_M, sizeof(char));
+    }
+
     int pcol, prow;
+    int l = 0;
     for(i = 1; i <= k; ++i) {
         for(j = 1; j <= k; ++j) {
-            int pid = phi(i, j, N, M);
+            int pid = phi(i, j);
             co_write(pid,
                     &hilb[k*(i-1) + (j-1)],
-                    0x4000, sizeof(float));
+                    LOC_MATRIX + sizeof(float) * l,
+                    sizeof(float));
         }
     }
 
@@ -74,17 +86,8 @@ int main(int argc, char **argv)
     // read L and U
     int pid = 0;
     for(pid = 0; pid < bsp_nprocs(); pid++) {
-        char msg;
-        int p;
-        int pcol, prow;
-        _get_p_coords(pid, &prow, &pcol);
-        e_read(&(_get_state()->dev),
-                prow, pcol,
-                (off_t)0x7050, &msg, 1);
-        e_read(&(_get_state()->dev),
-                prow, pcol,
-                (off_t)0x7100, &p, sizeof(int));
-        printf("%i: %c\n", p, msg);
+        //co_read
+        //printf
     }
 
     // show decomposition
