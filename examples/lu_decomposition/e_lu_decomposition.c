e5a7a428f7f2d7453afd7da731405fefc78ed3d1
Jan-Willem Buurlage <jw@Jan-Willems-MacBook-Air.local>
2015-01-06 12:37:10 +0100

    (LUD) convert between global and local indices, start work on finding pivot

--- examples/lu_decomposition/e_lu_decomposition.c
+++ examples/lu_decomposition/e_lu_decomposition.c
@@ -3,23 +3,69 @@
 
 #include "common.h"
 
+int M = 0;
+int N = 0;
+int s = 0;
+int t = 0;
+int dim = 0;
+int entries_per_row = 0;
+
+// "local to global" index
+int ltg(int& i, int& j, int l)
+{
+    i = s + (l % (dim / N)) * N;
+    j = t + (l % (dim / M)) * M;
+}
+
+// "global to local" index
+inline int gtl(int i, int j)
+{
+    // assume correct processor
+    return (i / (dim /  N)) * (dim / N) + (j / (dim / M));
+}
+
+inline float a(int i, int j) {
+    return *(LOC_MATRIX + gtl(i, j));
+}
+
 int main()
 {
+    int i, j, k;
     bsp_begin();
 
     int n = bsp_nprocs(); 
     int p = bsp_pid();
 
-    char* M = LOC_M;
+    M = (*LOC_M);
+    N = (*LOC_N);
+    dim = (*LOC_DIM);
+    entries_per_row = dim / M;
 
-    int s = p / (*M);
-    int t = p % (*M);
+    s = p / (*M);
+    t = p % (*M);
+
+    for (k = 0; k < dim; ++k) {
+        // stage k
+        if (k % M == t) {
+            int max_value = -1;
+            int rs = -1;
+            for (i = k; i < n; ++i) {
+                if (i % N == s) {
+                    int val = abs(a(i,k));
+                    if (val > max_value) {
+                        max_value = val;
+                        rs = i;
+                    }
+                }
+            }
+        }
+    }
 
     bsp_sync();
 
     int* result = LOC_RESULT;
     (*result) = s;
-    (*result + 1) = t;
+    (*(result + 1)) = t;
 
     return 0;
 }
