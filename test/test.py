d6cd058c68692d719205bb14440047be65b071b8
Abe Wits <abewits@gmail.com>
2015-04-24 09:47:35 +0000

    improved test.py

--- test/test.py
+++ test/test.py
@@ -2,6 +2,7 @@
 import re
 import subprocess
 import os
+import shlex
 
 EXPECT_PATTERN = re.compile(r'// expect: \((.*)\)')
 
@@ -21,6 +22,9 @@ def run_unit_test(unit_test):
         print("OSError")    #When running on non-epiphany system
     return output
 
+def clean_str(string):
+    return shlex.quote(string.replace("\n","\\n"))
+
 def do_unit_test(unit_test):
     host_srctext = get_contents("./"+unit_test+"/host_"+unit_test+".c")
     host_expected_outputs = re.findall(EXPECT_PATTERN, host_srctext)
@@ -30,15 +34,35 @@ def do_unit_test(unit_test):
 
     expected_output = "\n".join(host_expected_outputs)+"\n".join(e_expected_outputs);
     actual_output = run_unit_test(unit_test)
-
-    print("TESTING:" + unit_test)
-    print("GOT : " + actual_output)
-    print("WANT: " + expected_output)
     
+    succes = (actual_output == expected_output)
+    
+    if not succes:
+        print('WARNING: Unit test "' + unit_test + '" failed')
+        subprocess.call(['bash','-c','diff <(echo ' + clean_str(actual_output) + 
+                    ') <(echo ' + clean_str(expected_output) + ')'])
+    
+    return succes
 
 maketext = get_contents("Makefile")
 matches = re.search('all: (.*)\\n', maketext)
 unit_tests = matches.group(1).split()
 
+n_succes = 0
+n_fail = 0
 for unit_test in unit_tests:
-    do_unit_test(unit_test)
+    if do_unit_test(unit_test):
+        n_succes += 1
+    else:
+        n_fail += 1
+
+print("")
+print("Unit tests summary")
+print("------------------")
+if n_fail == 0:
+    print("SUCCES")
+    print("All "+str(n_succes)+" tests where a succes!")
+else:
+    print("FAILURE")
+    print(str(n_fail)+" tests failed, "+str(n_succes)+" tests succeeded")
+
