2a513f769a595f0649ad726d483cc20a135d0d03
Tom Bannink <tombannink@gmail.com>
2015-08-13 15:01:26 +0000

    Updated memory benchmark: separate case for the burst mode of the e-link

--- examples/memorybench/e_memorybench.c
+++ examples/memorybench/e_memorybench.c
@@ -14,12 +14,38 @@ unsigned int transfer(void* destination, const void* source, unsigned int count)
     return ebsp_raw_time();
 }
 
+// To avoid burst mode of the e-link
+unsigned int reverse_transfer(void* destination, const void* source, unsigned int count)
+{
+    ebsp_raw_time(); //reset timer
+    long long* dst = (long long*)destination;
+    const long long* src = (const long long*)source;
+    dst += count;
+    src += count;
+    while(count--)
+        *--dst = *--src;
+    return ebsp_raw_time();
+}
+
 // count is measured in longlongs
 unsigned int dma_transfer(void* destination, const void* source, unsigned int count)
 {
     ebsp_raw_time(); //reset timer
     e_dma_copy(destination, (void*)source, count*sizeof(long long));
     return ebsp_raw_time();
+}
+
+//Integrity check
+void check_buffer(const long long* buffer, unsigned int count)
+{
+    for (unsigned int i = 0; i < count;  i++)
+    {
+        if (buffer[i] != i)
+        {
+            bsp_abort("Buffer %p is corrupted!!!", buffer);
+            break;
+        }
+    }
 }
 
 int main()
@@ -31,11 +57,15 @@ int main()
     //
     // Core <-> External memory
     //
-    const long chunk_size = 2048;
+    const long chunk_size = 4096;
     const long dword_count = chunk_size / sizeof(long long);
 
     long long* extmem_buffer = ebsp_ext_malloc(chunk_size);
     long long local_buffer[dword_count];
+
+    // Integrity checks
+    for (int i = 0; i < dword_count; i++)
+        local_buffer[i] = i;
 
     const int samples = 1;
     int timings[dword_count+1];
@@ -43,7 +73,7 @@ int main()
     // Write speed, one core at a time
     
     if (p == 0) {
-        ebsp_message("write extmem nodma nonbusy");
+        ebsp_message("write extmem nodma burst");
     }
     for (int iter=0; iter < samples; iter++)
     {
@@ -57,6 +87,26 @@ int main()
             }
         }
     }
+
+    if (p == 0) {
+        ebsp_message("write extmem nodma nonbusy");
+    }
+    for (int iter=0; iter < samples; iter++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            bsp_sync();
+            if (i != p) continue;
+            for (int h = 0; h <= dword_count; h++) {
+                int timer = reverse_transfer(extmem_buffer, local_buffer, h);
+                ebsp_message("%d %d", h, timer);
+            }
+        }
+    }
+
+
+    check_buffer(extmem_buffer, dword_count);
+    check_buffer(local_buffer, dword_count);
 
     // Read speed, one core at a time
 
@@ -77,6 +127,9 @@ int main()
         }
     }
 
+    check_buffer(extmem_buffer, dword_count);
+    check_buffer(local_buffer, dword_count);
+
     // Write speed, all cores at the same time
 
     bsp_sync();
@@ -92,7 +145,10 @@ int main()
         for (int h = 0; h <= dword_count; h++ )
             ebsp_message("%d %d", h, timings[h]);
     }
-    
+
+    check_buffer(extmem_buffer, dword_count);
+    check_buffer(local_buffer, dword_count);
+
     // Read speed, all cores at the same time
 
     bsp_sync();
@@ -108,6 +164,9 @@ int main()
         for (int h = 0; h <= dword_count; h++ )
             ebsp_message("%d %d", h, timings[h]);
     }
+
+    check_buffer(extmem_buffer, dword_count);
+    check_buffer(local_buffer, dword_count);
 
     //========================================================================
     // DMA
@@ -132,26 +191,34 @@ int main()
         }
     }
 
+    check_buffer(extmem_buffer, dword_count);
+    check_buffer(local_buffer, dword_count);
+
     // Read speed, one core at a time
 
-    bsp_sync();
-    if (p == 0) {
-        ebsp_message("read extmem dma nonbusy");
-    }
-    for (int iter=0; iter < samples; iter++)
-    {
-        for (int i = 0; i < n; i++)
-        {
-            bsp_sync();
-            if (i != p) continue;
-            for (int h = 0; h <= dword_count; h++) {
-                int timer = dma_transfer(local_buffer, extmem_buffer, h);
-                ebsp_message("%d %d", h, timer);
-            }
-        }
-    }
+//TODO: Figure out why DMA never returns
+//    bsp_sync();
+//    if (p == 0) {
+//        ebsp_message("read extmem dma nonbusy");
+//    }
+//
+//    for (int iter=0; iter < samples; iter++)
+//    {
+//        for (int i = 0; i < n; i++)
+//        {
+//            bsp_sync();
+//            if (i != p) continue;
+//            for (int h = 0; h <= dword_count; h++) {
+//                int timer = transfer(local_buffer, extmem_buffer, h);
+//                ebsp_message("%d %d", h, timer);
+//            }
+//        }
+//    }
+//
+//    check_buffer(extmem_buffer, dword_count);
+//    check_buffer(local_buffer, dword_count);
 
-    // Write speed, all cores at the same time
+    //// Write speed, all cores at the same time
 
     bsp_sync();
     if (p == 0) {
@@ -166,23 +233,26 @@ int main()
         for (int h = 0; h <= dword_count; h++ )
             ebsp_message("%d %d", h, timings[h]);
     }
+
+    check_buffer(extmem_buffer, dword_count);
+    check_buffer(local_buffer, dword_count);
     
     // Read speed, all cores at the same time
 
-    bsp_sync();
-    if (p == 0) {
-        ebsp_message("read extmem dma busy");
-    }
-    for (int iter=0; iter < samples; iter++) {
-        for (int h = 0; h <= dword_count; h++) {
-            bsp_sync();
-            timings[h] = dma_transfer(local_buffer, extmem_buffer, h);
-            bsp_sync();
-        }
-        for (int h = 0; h <= dword_count; h++ )
-            ebsp_message("%d %d", h, timings[h]);
-    }
-
+//    bsp_sync();
+//    if (p == 0) {
+//        ebsp_message("read extmem dma busy");
+//    }
+//    for (int iter=0; iter < samples; iter++) {
+//        for (int h = 0; h <= dword_count; h++) {
+//            bsp_sync();
+//            timings[h] = transfer(local_buffer, extmem_buffer, h);
+//            bsp_sync();
+//        }
+//        for (int h = 0; h <= dword_count; h++ )
+//            ebsp_message("%d %d", h, timings[h]);
+//    }
+//
     bsp_end();
     
     return 0;
