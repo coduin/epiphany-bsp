e8f6184ff5f09d3d8f8170fcc9aa804e34cd38d4
Tom Bannink <tombannink@gmail.com>
2015-08-14 19:34:57 +0000

    DMA bug found: reading 0 bytes with MSGMODE enabled stalls DMA. Host to extmem benchmark is dissapointing.

--- examples/memorybench/e_memorybench.c
+++ examples/memorybench/e_memorybench.c
@@ -27,11 +27,60 @@ unsigned int reverse_transfer(void* destination, const void* source, unsigned in
     return ebsp_raw_time();
 }
 
+//
+// Taken from e_dma_copy source
+//
+
+extern unsigned dma_data_size[8];
+
+#define local_mask (0xfff00000)
+
+extern e_dma_desc_t _dma_copy_descriptor_;
+
+int dma_copy(void *dst, void *src, size_t n)
+{
+    e_dma_id_t chan;
+    unsigned   index;
+    unsigned   shift;
+    unsigned   stride;
+    unsigned   config;
+    int        ret_val;
+
+    if (n == 0) return 0;
+
+    chan  = E_DMA_0;
+
+    index = (((unsigned) dst) | ((unsigned) src) | ((unsigned) n)) & 7;
+
+    config = E_DMA_MASTER | E_DMA_ENABLE | dma_data_size[index];
+    if ((((unsigned) dst) & local_mask) == 0)
+        config = config | E_DMA_MSGMODE;
+    shift = dma_data_size[index] >> 5;
+    stride = 0x10001 << shift;
+
+    // TODO: add e_dma_wait()!!!
+    _dma_copy_descriptor_.config       = config;
+    _dma_copy_descriptor_.inner_stride = stride;
+    _dma_copy_descriptor_.count        = 0x10000 | (n >> shift);
+    _dma_copy_descriptor_.outer_stride = 0; // stride;
+    _dma_copy_descriptor_.src_addr     = src;
+    _dma_copy_descriptor_.dst_addr     = dst;
+
+    ret_val = e_dma_start(&_dma_copy_descriptor_, chan);
+
+    volatile unsigned* reg_addr = e_get_global_address(e_group_config.core_row, e_group_config.core_col, (void*)E_REG_DMA0STATUS);
+
+    while ( (*reg_addr) & 0xf );
+
+    return ret_val;
+}
+
+
 // count is measured in longlongs
 unsigned int dma_transfer(void* destination, const void* source, unsigned int count)
 {
     ebsp_raw_time(); //reset timer
-    e_dma_copy(destination, (void*)source, count*sizeof(long long));
+    dma_copy(destination, (void*)source, count*sizeof(long long));
     return ebsp_raw_time();
 }
 
@@ -57,7 +106,7 @@ int main()
     //
     // Core <-> External memory
     //
-    const long chunk_size = 4096;
+    const long chunk_size = 2048;
     const long dword_count = chunk_size / sizeof(long long);
 
     long long* extmem_buffer = ebsp_ext_malloc(chunk_size);
@@ -72,6 +121,7 @@ int main()
 
     // Write speed, one core at a time
     
+    bsp_sync();
     if (p == 0) {
         ebsp_message("write extmem nodma burst");
     }
@@ -88,6 +138,10 @@ int main()
         }
     }
 
+    check_buffer(extmem_buffer, dword_count);
+    check_buffer(local_buffer, dword_count);
+
+    bsp_sync();
     if (p == 0) {
         ebsp_message("write extmem nodma nonbusy");
     }
@@ -103,7 +157,6 @@ int main()
             }
         }
     }
-
 
     check_buffer(extmem_buffer, dword_count);
     check_buffer(local_buffer, dword_count);
@@ -196,27 +249,26 @@ int main()
 
     // Read speed, one core at a time
 
-//TODO: Figure out why DMA never returns
-//    bsp_sync();
-//    if (p == 0) {
-//        ebsp_message("read extmem dma nonbusy");
-//    }
-//
-//    for (int iter=0; iter < samples; iter++)
-//    {
-//        for (int i = 0; i < n; i++)
-//        {
-//            bsp_sync();
-//            if (i != p) continue;
-//            for (int h = 0; h <= dword_count; h++) {
-//                int timer = transfer(local_buffer, extmem_buffer, h);
-//                ebsp_message("%d %d", h, timer);
-//            }
-//        }
-//    }
-//
-//    check_buffer(extmem_buffer, dword_count);
-//    check_buffer(local_buffer, dword_count);
+    bsp_sync();
+    if (p == 0) {
+        ebsp_message("read extmem dma nonbusy");
+    }
+
+    for (int iter=0; iter < samples; iter++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            bsp_sync();
+            if (i != p) continue;
+            for (int h = 0; h <= dword_count; h++) {
+                int timer = dma_transfer(local_buffer, extmem_buffer, h);
+                ebsp_message("%d %d", h, timer);
+            }
+        }
+    }
+
+    check_buffer(extmem_buffer, dword_count);
+    check_buffer(local_buffer, dword_count);
 
     //// Write speed, all cores at the same time
 
@@ -239,20 +291,20 @@ int main()
     
     // Read speed, all cores at the same time
 
-//    bsp_sync();
-//    if (p == 0) {
-//        ebsp_message("read extmem dma busy");
-//    }
-//    for (int iter=0; iter < samples; iter++) {
-//        for (int h = 0; h <= dword_count; h++) {
-//            bsp_sync();
-//            timings[h] = transfer(local_buffer, extmem_buffer, h);
-//            bsp_sync();
-//        }
-//        for (int h = 0; h <= dword_count; h++ )
-//            ebsp_message("%d %d", h, timings[h]);
-//    }
-//
+    bsp_sync();
+    if (p == 0) {
+        ebsp_message("read extmem dma busy");
+    }
+    for (int iter=0; iter < samples; iter++) {
+        for (int h = 0; h <= dword_count; h++) {
+            bsp_sync();
+            timings[h] = dma_transfer(local_buffer, extmem_buffer, h);
+            bsp_sync();
+        }
+        for (int h = 0; h <= dword_count; h++ )
+            ebsp_message("%d %d", h, timings[h]);
+    }
+
     bsp_end();
     
     return 0;
--- examples/memorybench/host_memorybench.c
+++ examples/memorybench/host_memorybench.c
@@ -1,7 +1,35 @@
 #include <host_bsp.h>
 #include <stdio.h>
 #include <stdlib.h>
- 
+#include <string.h>
+
+//#define BENCH_HOST
+
+#ifdef BENCH_HOST
+
+#define __USE_XOPEN2K
+#define __USE_POSIX199309 1
+#include <time.h>
+
+struct timespec ts_start, ts_end;
+
+void reset()
+{
+    clock_gettime(CLOCK_MONOTONIC, &ts_start);
+}
+
+long long getElapsedNanoSec()
+{
+    clock_gettime(CLOCK_MONOTONIC, &ts_end);
+    return ((ts_end.tv_sec - ts_start.tv_sec) * 1e9  +
+            (ts_end.tv_nsec - ts_start.tv_nsec));
+}
+
+void* ebsp_ext_malloc(unsigned int nbytes);
+void ebsp_free(void* ptr);
+
+#endif
+
 int main(int argc, char **argv)
 {
     if(!bsp_init("e_memorybench.srec", argc, argv)) {
@@ -12,7 +40,68 @@ int main(int argc, char **argv)
         fprintf(stderr, "[BSPBENCH] bsp_begin() failed\n");
     }
 
+#ifdef BENCH_HOST
+    long long time_loop, time_memcpy, time_flops;
+
+    const unsigned chunk_size = 4*1024*1024;
+    void* ext_ptr = ebsp_ext_malloc(chunk_size);
+    void* local_ptr = malloc(chunk_size);
+
+    if (!ext_ptr)
+    {
+        fprintf(stderr, "extmem_malloc error");
+    }
+    else if (!local_ptr)
+    {
+        fprintf(stderr, "local malloc error");
+    }
+    else
+    {
+        // Timing: copy to external memory in a long long loop
+        {
+            const unsigned dword_count = chunk_size / sizeof(long long);
+            long long* dst = (long long*)ext_ptr;
+            long long* src = (long long*)local_ptr;
+            reset();
+            for (unsigned i = 0; i < dword_count; i++)
+                *dst++ = *src++;
+            time_loop = getElapsedNanoSec();
+        }
+
+        // Timing: memcpy to external memory
+        {
+            reset();
+            memcpy(ext_ptr, local_ptr, chunk_size);
+            time_memcpy = getElapsedNanoSec();
+        }
+
+        // Timing: floating point operations
+        {
+            float* floats = (float*)local_ptr;
+            unsigned float_count = chunk_size / sizeof(float);
+
+            reset();
+            floats[0] = 1.0f;
+            floats[1] = 1.0f;
+            floats[2] = 1.0f;
+            for (unsigned i = 3; i < float_count; i++)
+            {
+                floats[i] = floats[i-1] + floats[i-2] * floats[i-3];
+            }
+            time_flops = getElapsedNanoSec();
+        }
+
+        ebsp_free(ext_ptr);
+        free(local_ptr);
+    }
+
+    printf("Host timing info:\n");
+    printf("copy by loop:\t%lld ns\t%fms\n",time_loop,1e-6f*time_loop);
+    printf("copy by memcpy:\t%lld ns\t%fms\n",time_memcpy,1e-6f*time_memcpy);
+    printf("flops loop:\t%lld ns\t%fms\n",time_flops,1e-6f*time_flops);
+#else
     ebsp_spmd();
+#endif
     bsp_end();
     
     return 0;
